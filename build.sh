e31072480cc2284f7302f244d54ae5b89832ee36
jinlinbi <898141731@qq.com>
2020-05-31 18:11:07 +0800

    fix gitlab-ci.yml error

--- build.sh
+++ build.sh
@@ -9,10 +9,26 @@ cd skynet-ext
 make linux
 cd ..
 
-cp skynet-ext/luaclib/* bin/luaclib
-cp skynet-ext/cservice/*.so bin/cservice
-cp skynet/luaclib/*.so bin/luaclib
-cp skynet/cservice/*.so bin/cservice
-cp -r skynet/lualib/* bin/lualib
-cp -r skynet/service/* bin/service
+if [ ! -d bin/lualib ]; then
+    mkdir bin/lualib
+fi
+
+if [ ! -d bin/luaclib ]; then
+    mkdir bin/luaclib
+fi
+
+if [ ! -d bin/cservice ]; then
+    mkdir bin/cservice
+fi
+
+if [ ! -d bin/service ]; then
+    mkdir bin/service
+fi
+
+cp skynet-ext/luaclib/* bin/luaclib/
+cp skynet-ext/cservice/*.so bin/cservice/
+cp skynet/luaclib/*.so bin/luaclib/
+cp skynet/cservice/*.so bin/cservice/
+cp -r skynet/lualib/* bin/lualib/
+cp -r skynet/service/* bin/service/
 cp skynet/skynet bin/
