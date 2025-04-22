1638aaa6e335d80d992193de30500dc9159bd3eb
jinlinbi <898141731@qq.com>
2025-04-22 09:51:56 +0800

    etcd api测试

--- myservice/testetcd.lua
+++ myservice/testetcd.lua
@@ -9,7 +9,8 @@ skynet.start(function()
         -- user = user,
         -- password = password,
         protocol = "v3",
-        serializer ="json"  -- 默认使用json格式配置
+        serializer ="json",  -- 默认使用json格式配置
+        ttl = 3,
     })
 
     if not etcdCli then
@@ -17,12 +18,22 @@ skynet.start(function()
         return
     end
 
-    log_info("set hellworld", inspect(etcdCli:set("/skynet/node1", {helloworld = "niviaiiaiddkdjj"})))
-    log_info("set hellworld", inspect(etcdCli:set("/skynet/node2", {helloworld = "不哈黄河a但凡扥经济法"})))
-    log_info("get /skynet/node1", inspect(etcdCli:get("/skynet/node1")))
-    log_info("get /skynet/node2", inspect(etcdCli:get("/skynet/node2")))
-    log_info("delete /skynet/node1", inspect(etcdCli:delete("/skynet/node1")))
-    log_info("delete /skynet/node2", inspect(etcdCli:delete("/skynet/node2")))
-    log_info("get /skynet/node1", inspect(etcdCli:get("/skynet/node1")))
-    log_info("get /skynet/node2", inspect(etcdCli:get("/skynet/node2")))
+    local body = etcdCli:grant(10).body
+    local ID = body.ID
+    local TTL = body.TTL
+    log_info("TTL", TTL)
+    log_info("ID", ID)
+    log_info("set hellworld", inspect(etcdCli:set("/skynet/node1", {helloworld = "niviaiiaiddkdjj"}, {lease = ID})))
+
+
+    -- log_info("get /skynet/node1", inspect(etcdCli:get("/skynet/node1")))
+    -- log_info("get /skynet/node2", inspect(etcdCli:get("/skynet/node2")))
+    -- log_info("set hellworld", inspect(etcdCli:set("/skynet/node2", {helloworld = "不哈黄河a但凡扥经济法"})))
+    -- log_info("get /skynet/node1", inspect(etcdCli:get("/skynet/node1")))
+    -- log_info("get /skynet/node2", inspect(etcdCli:get("/skynet/node2")))
+    -- log_info("delete /skynet/node1", inspect(etcdCli:delete("/skynet/node1")))
+    -- log_info("delete /skynet/node2", inspect(etcdCli:delete("/skynet/node2")))
+    -- log_info("get /skynet/node1", inspect(etcdCli:get("/skynet/node1")))
+    -- log_info("get /skynet/node2", inspect(etcdCli:get("/skynet/node2")))
+    -- log_info("get /skynet/node", inspect(etcdCli:setx("/skynet/node", {hello = "world"}, {timeout = 3})))
 end)
